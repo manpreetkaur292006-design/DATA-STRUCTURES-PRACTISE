4a5532a2def1d5f29ec438939ae9bdc2f950b491
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-02-01 15:07:32 +0530

    Added three .py files

---
+++ stackADT.py
@@ -0,0 +1,10 @@
+class StackADT:
+    def __init__(self):
+        self.data=[]
+    def push(self,n):
+        return self.data.append(n)
+    def pop(self):
+    def peek(self):
+    def display(self):
+    def isEmpty(self):
+    def size(self):
