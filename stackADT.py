a6af23e13aeecf3899fe081e98f5bfbf60c54af7
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-02-01 15:14:05 +0530

    made a class and defined all the related functions in the file

--- stackADT.py
+++ stackADT.py
@@ -1,10 +1,25 @@
 class StackADT:
     def __init__(self):
         self.data=[]
-    def push(self,n):
-        return self.data.append(n)
-    def pop(self):
-    def peek(self):
-    def display(self):
     def isEmpty(self):
+        if len(self.data)==0:
+            return True
+        else:
+            return False
+    def push(self,n):
+        if self.isEmpty()==True:
+            return "Stack underflow !"
+        else:
+            return self.data.append(n)
+    def pop(self):
+        if self.isEmpty()==True:
+            return "Stack underflow !"
+        else:
+            return self.data.pop()
+    def peek(self):
+        return self.data[-1]
+    def display(self):
+        return self.data
     def size(self):
+        return len(self.data)
+
