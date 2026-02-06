87977d72c4a87ee25cb5ffdcd67a4692d5aed885
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-02-06 21:20:01 +0530

    added the solution of the question 2

--- 6-FEB-TEST-SOL.py
+++ 6-FEB-TEST-SOL.py
@@ -28,3 +28,16 @@ print(factorial(n))
 
 # ANSWER :
 
+arr=[1,2,3,4,5]
+even=0
+odd=0
+for i in range(len(arr)):
+    if arr[i]%2==0:
+        even+=1
+    else:
+        odd+=1
+count_lst=[]
+count_lst.append(even)
+count_lst.append(odd)
+print(count_lst)
+
