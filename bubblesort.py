4079ef2365a28bdcb67a9f06cb1500ec940dae68
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-03-27 11:20:53 +0530

    added some spaces

--- bubblesort.py
+++ bubblesort.py
@@ -1,11 +1,17 @@
 # BUBBLE SORT
 
 def bubble_sort(arr): # DEFINING THE FUNCTION
+
     n=len(arr)  # STORING LENGTH IN A VARIABLE
+
     for i in range(n):  # FOR LOOP TILL THE RANGE N
+
         for j in range(0,n-i-1):  # FOR LOOP TILL THE RANGE N-I-1
+            
             if arr[j]>arr[j+1]:  # COMPARING THE ELEMENTS
+
                 arr[j],arr[j+1]=arr[j+1],arr[j]  # SWAPPING
+                
     return arr  # RETURNING THE OUTPUT
 
 print(bubble_sort([3,1,4,2]))  # PRINTING THE OUTPUT AND CALLING THE FUNCTION
--- selectionsort.py
+++ selectionsort.py
@@ -1,12 +1,21 @@
 # SELECTION SORT
 
 def selection_sort(arr):  # FUNCTION DEFINITION
+
     n=len(arr)  # STORING LENGTH IN THE VARIABLE
+
     for i in range(n-1):  # RUNNING FOR LOOP TILL THE RANGE N-1
+
         min_idx=i  # SETTING THE MINIMUM INDEX TO I
+
         for j in range(i+1,n): # RUNNING ANOTHER LOOP FOR COMPARISON
+
             if arr[j]<arr[min_idx]:  # COMPARING THE CURRENT ELEMENT WITH THE MIN_IDX ELEMENT
+
                 min_idx=j  # SETTING THE MIN IDX TO J IF THE CONDITION SATISFIES
+
         arr[i],arr[min_idx]=arr[min_idx],arr[i]  # SWAPPING
+
     return arr  # RETURNING THE ARRAY 
+
 print(selection_sort([2,3,1,4]))  # PRINTING AND CALLING THE FUNCTION
